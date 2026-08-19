4762b3b89f30e090d55bdf1ad4a636b2c346c2a0
Denis <zalizkodenis12@gmail.com>
2026-08-19 14:55:20 +0300

    Update history section image, fix title font size, remove FB link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -58,14 +58,6 @@ export default function Footer() {
                 <span className="font-black text-[11px] sm:text-xs md:text-xl text-[#FFFFFF]">@daocleanvinnytsia</span>
               </a>
 
-              {/* Facebook */}
-              <a href="https://www.facebook.com/" target="_blank" rel="noreferrer" className="group flex items-center gap-2 md:gap-4 hover:-translate-y-1 transition-transform text-left">
-                <div className="w-8 h-8 md:w-14 md:h-14 bg-white text-[#0158C4] rounded-full flex items-center justify-center shadow-md group-hover:shadow-lg transition-shadow shrink-0">
-                  <svg className="w-4 h-4 md:w-7 md:h-7" fill="currentColor" viewBox="0 0 24 24"><path d="M22.675 0h-21.35c-.732 0-1.325.593-1.325 1.325v21.351c0 .731.593 1.324 1.325 1.324h11.495v-9.294h-3.128v-3.622h3.128v-2.671c0-3.1 1.893-4.788 4.659-4.788 1.325 0 2.463.099 2.795.143v3.24l-1.918.001c-1.504 0-1.795.715-1.795 1.763v2.312h3.587l-.467 3.622h-3.12v9.293h6.116c.73 0 1.323-.593 1.323-1.325v-21.35c0-.732-.593-1.325-1.325-1.325z"/></svg>
-                </div>
-                <span className="font-black text-[11px] sm:text-xs md:text-xl text-[#FFFFFF]">DaO Clean Вінниця</span>
-              </a>
-
               {/* TikTok */}
               <a href="https://tiktok.com/@daocleanvinnusya" target="_blank" rel="noreferrer" className="group flex items-center gap-2 md:gap-4 hover:-translate-y-1 transition-transform text-left">
                 <div className="w-8 h-8 md:w-14 md:h-14 bg-white text-[#0158C4] rounded-full flex items-center justify-center shadow-md group-hover:shadow-lg transition-shadow shrink-0">
